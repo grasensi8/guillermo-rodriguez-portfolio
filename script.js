c572c6c276e8b18b114fa5da1df769eb730d6afb
Guillermo Rodríguez <grasensi8@gmail.com>
2025-07-22 14:07:37 +0200

    aÑADIDA UNA IMAGEN NUEVA COMO PRUEBA ES LA TAPA DE UNIDO DE BRAND GLOBAL SUMMIT. aÑADIDO LA PAGINA DE SERVICIOS CREADA COMPLETA CASI TODO, FALTA REVISION Y AÑADIR PORTADAS PROYECTOS.

--- script.js
+++ script.js
@@ -95,3 +95,16 @@ const observer = new IntersectionObserver((entries) => {
 document.querySelectorAll('.scroll-flip-down').forEach(el => {
   observer.observe(el);
 });
+
+
+//Animación de servicios show room portfolio
+  const container = document.querySelector('.projects_cards__show_container');
+  const scrollAmount = 350;
+
+  document.querySelector('.left-arrow').addEventListener('click', () => {
+    container.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+  });
+
+  document.querySelector('.right-arrow').addEventListener('click', () => {
+    container.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+  });
